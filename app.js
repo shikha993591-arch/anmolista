0d960c63c617110c7c1f541ebec57db40fb70520
shikha993591-arch <shikha993591@gmail.com>
2026-01-10 01:39:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 firebase.initializeApp({
-  apiKey:"YOUR_KEY",
+  apiKey:"YOUR_API_KEY",
   authDomain:"anmolista.firebaseapp.com",
   projectId:"anmolista"
 });
 
-const db=firebase.firestore();
 const auth=firebase.auth();
+const db=firebase.firestore();
+
+// LOGIN
+function login(){
+ auth.signInWithEmailAndPassword(email.value,password.value)
+ .then(checkProfile)
+ .catch(()=>auth.createUserWithEmailAndPassword(email.value,password.value)
+ .then(checkProfile));
+}
+
+function checkProfile(){
+ const u=auth.currentUser;
+ db.collection("users").doc(u.uid).get().then(d=>{
+  location=d.exists?"home.html":"setup.html";
+ });
+}
 
 // SAVE PROFILE
 function saveProfile(){
@@ -13,30 +28,50 @@ function saveProfile(){
  db.collection("users").doc(u.uid).set({
   name:name.value,
   username:username.value,
-  bio:bio.value,
-  online:true,
-  lastSeen:Date.now()
- });
- alert("Saved");
+  bio:bio.value
+ }).then(()=>location="home.html");
 }
 
-// SEND MESSAGE
+// SEARCH USER
+if(document.getElementById("searchUser")){
+ searchUser.oninput=()=>{
+  db.collection("users").where("username","==",searchUser.value)
+  .get().then(s=>{
+   searchResult.innerHTML="";
+   s.forEach(d=>{
+    searchResult.innerHTML+=`<div onclick="startChat('${d.id}','${d.data().username}')">${d.data().username}</div>`;
+   });
+  });
+ };
+}
+
+// START CHAT
+function startChat(uid,username){
+ localStorage.chatWith=uid;
+ localStorage.chatName=username;
+ location="chat.html";
+}
+
+// CHAT
 function sendMessage(){
- const chatId=localStorage.chatId;
- db.collection("messages").doc(chatId)
- .collection("chat").add({
+ const chatId=[auth.currentUser.uid,localStorage.chatWith].sort().join("_");
+ db.collection("messages").doc(chatId).collection("chat").add({
   text:msg.value,
   sender:auth.currentUser.uid,
   time:Date.now()
  });
  msg.value="";
+ db.collection("chats").doc(chatId).set({
+  users:[auth.currentUser.uid,localStorage.chatWith],
+  lastMessage:msg.value
+ });
 }
 
-// LISTEN CHAT
-function loadChat(chatId){
- db.collection("messages").doc(chatId)
- .collection("chat").orderBy("time")
- .onSnapshot(s=>{
+if(document.getElementById("messages")){
+ chatWith.innerText=localStorage.chatName;
+ const chatId=[auth.currentUser.uid,localStorage.chatWith].sort().join("_");
+ db.collection("messages").doc(chatId).collection("chat")
+ .orderBy("time").onSnapshot(s=>{
   messages.innerHTML="";
   s.forEach(d=>{
    let div=document.createElement("div");
@@ -46,3 +81,5 @@ function loadChat(chatId){
   });
  });
 }
+
+function goHome(){location="home.html";}
