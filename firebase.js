a0533d9601e811676f045f41bdd4838df2824b94
shikha993591-arch <shikha993591@gmail.com>
2026-01-10 13:22:14 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBpF3Q1ESL76ywrp2_OVOoOJiFOv322z5M",
+  authDomain: "anmolista.firebaseapp.com",
+  projectId: "anmolista",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+</script>
